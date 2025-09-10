bbcd39e2cef11b1ab1235df3b01e0678a38308c9
mobile-agent-release <>
2025-09-10 13:15:51 +0200

    8.323.1 release

--- Package.swift
+++ Package.swift
@@ -13,9 +13,6 @@ let package = Package(
             name: "Dynatrace",
             targets: ["Dynatrace-Dynamic"]),
         .library(
-            name: "Dynatrace-Static",
-            targets: ["Dynatrace-Static"]),
-        .library(
             name: "DynatraceSessionReplay",
             targets: ["Dynatrace-Dynamic", "DynatraceSessionReplay"]),
     ],
@@ -26,25 +23,13 @@ let package = Package(
         ),
         .binaryTarget(
             name: "Dynatrace",
-            url: "https://mobileagent.downloads.dynatrace.com/ios/8.321.2.1012/dynatrace-mobile-agent-ios-8.321.2.1012-xcframework.zip",
-            checksum: "ee456d9476d63f0e356ff3eb1d5aeb1949930d35b4ace0ee7403197591e9a27d"
-        ),
-        .target( // wrap target to add linker settings
-            name: "Dynatrace-Static",
-            dependencies: ["DynatraceStatic"],
-            linkerSettings: [.linkedLibrary("c++")/*, .unsafeFlags(["-ObjC"])*/]    //security restrictions currently don't allow unsafe flags here, needs to be added to Other Linker Flags manually
-            //for more details read https://github.com/apple/swift-evolution/blob/master/proposals/0238-package-manager-build-settings.md#unsafe-flags-all
-            //or https://forums.swift.org/t/confused-by-unsafe-flags-being-disallowed-in-dependencies/27359
-        ),
-        .binaryTarget(
-            name: "DynatraceStatic",
-            url: "https://mobileagent.downloads.dynatrace.com/ios/8.321.2.1012/dynatrace-mobile-agent-ios-8.321.2.1012-xcframework-static.zip",
-            checksum: "087ca020452abbfe7b771299578196ae1d21173da6df1691b9b47a8fdbba5b64"
+            url: "https://mobileagent.downloads.dynatrace.com/ios/8.323.1.1009/dynatrace-mobile-agent-ios-8.323.1.1009-xcframework.zip",
+            checksum: "0979e2cf5f1b204a7e8af422fdd7c16a57a9757216311a58b08e3da1a961e3e2"
         ),
         .binaryTarget(
             name: "DynatraceSessionReplay",
-            url: "https://mobileagent.downloads.dynatrace.com/ios/8.321.2.1012/dynatrace-mobile-agent-ios-8.321.2.1012-replay-xcframework.zip",
-            checksum: "da9def5946d3ffc668aced4f300b10cd6036f78bc8aca181511533d805bb03e1"
+            url: "https://mobileagent.downloads.dynatrace.com/ios/8.323.1.1009/dynatrace-mobile-agent-ios-8.323.1.1009-replay-xcframework.zip",
+            checksum: "ffa2be2621c578a196bd363c8bdc5ea75d72afc51b7f94482f9144b1b1b0be0d"
         ),
     ]
 )
