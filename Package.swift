e0af2193d496a6f719e804adc7a929199f9d7c42
mobile-agent-release <>
2026-01-21 08:52:35 +0100

    8.329.2 release

--- Package.swift
+++ Package.swift
@@ -23,12 +23,12 @@ let package = Package(
         ),
         .binaryTarget(
             name: "Dynatrace",
-            url: "https://mobileagent.downloads.dynatrace.com/ios/8.329.1.1017/dynatrace-mobile-agent-ios-8.329.1.1017-xcframework.zip",
-            checksum: "d6d693f48994225b9252ee13aef7932be0374bce7fe691dd6c2c5588cefb4562"
+            url: "https://mobileagent.downloads.dynatrace.com/ios/8.329.2.1019/dynatrace-mobile-agent-ios-8.329.2.1019-xcframework.zip",
+            checksum: "e1ea3a8eaeeee2c385996e718e081f03d73d46ffb7b61d79a1473fc851d04bb2"
         ),
         .binaryTarget(
             name: "DynatraceSessionReplay",
-            url: "https://mobileagent.downloads.dynatrace.com/ios/8.329.1.1017/dynatrace-mobile-agent-ios-8.329.1.1017-replay-xcframework.zip",
+            url: "https://mobileagent.downloads.dynatrace.com/ios/8.329.2.1019/dynatrace-mobile-agent-ios-8.329.2.1019-replay-xcframework.zip",
             checksum: "27e79cbadb8cf7229ee990d4d266c7ee2f75b1f2aaf019322a01ea8da1d80ac1"
         ),
     ]
